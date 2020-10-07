312c7a214b4bcb3741e15f70442c99a6d31dc658
Yusuke Hattori <y-hattori@groovenauts.jp>
2020-10-07 17:51:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Slackに電話番号(+811234567890形式)が貼られたらSansan APIから電話番号検索して、ついでにjpnumberのリンクを貼るbot。
+
+## Usage
+
+```
+bundle
+TOKEN='<SLACK Bot User OAuth Access Token>' SANSAN_API_KEY='<Sansan Api Key>' bundle exec ruby slackbot-phonenumber.rb
+```
---
+++ slackbot-phonenumber.rb
@@ -0,0 +1,63 @@
+require 'slack-ruby-client'
+require 'net/http'
+require 'uri'
+require 'pry'
+
+TOKEN = ENV['TOKEN']
+SANSAN_API_KEY = ENV['SANSAN_API_KEY']
+
+Slack.configure do |config|
+  config.token = TOKEN
+end
+
+client = Slack::RealTime::Client.new
+
+def phonenumber_search(n)
+  uri = URI.parse("https://api.sansan.com/v2.5/bizCards/search?limit=1&tel=#{n}")
+  request = Net::HTTP::Get.new(uri)
+  request["X-Sansan-Api-Key"] = SANSAN_API_KEY
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+
+  if response.code == '200'
+    j = JSON(response.body)['data']
+    if j.size > 0
+      d = j.first
+      "#{d['departmentName']}の#{d['lastName']} #{d['firstName']}様 tel:#{d['tel']}"
+    else
+      nil
+    end
+  end
+end
+
+client.on :hello do
+  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
+end
+
+client.on :message do |data|
+  case data.text
+  when 'bot hi' then
+    client.message(channel: data.channel, text: "Hi <@#{data.user}>!")
+  when /(\+[\-\d]{3,}\d)/ then
+    n = $1.gsub(/^\+81/, '0')
+    result = phonenumber_search(n)
+    result = "なし" unless result
+    client.message(channel: data.channel, text: "Sansan 検索結果: #{result}\njpnumber検索: https://www.jpnumber.com/searchnumber.do?number=#{n}")
+  end
+end
+
+client.on :close do |_data|
+  puts "Client is about to disconnect"
+end
+
+client.on :closed do |_data|
+  puts "Client has disconnected successfully!"
+end
+
+client.start!
